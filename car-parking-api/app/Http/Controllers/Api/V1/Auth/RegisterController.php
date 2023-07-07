903947de89d55ac7eaf8b6cd0dfec338c86b35d1
Vitaliy Semenov <synthex@bk.ru>
2023-07-07 20:59:54 +0600

    feat: register new user

---
+++ car-parking-api/app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\Auth\UserResource;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegisterController extends Controller
+{
+    public function __invoke(RegisterRequest $request): JsonResource
+    {
+        $user = User::create($request->validated());
+
+        event(new Registered($user));
+
+        return new UserResource($user);
+    }
+}
