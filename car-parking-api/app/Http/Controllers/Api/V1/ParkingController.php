729860f6b673690827f809898fcf444e3d022b65
Vitaliy Semenov <synthex@bk.ru>
2023-09-24 00:32:00 +0600

    fix: add validation if parking already stopped

--- car-parking-api/app/Http/Controllers/Api/V1/ParkingController.php
+++ car-parking-api/app/Http/Controllers/Api/V1/ParkingController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\StartParkingRequest;
 use App\Http\Resources\ParkingResource;
 use App\Models\Parking;
 use App\Services\ParkingPriceService;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -32,8 +31,13 @@ class ParkingController extends Controller
         return ParkingResource::make($parking);
     }
 
-    public function stop(Parking $parking, ParkingPriceService $parkingPriceService): JsonResource
+    public function stop(Parking $parking, ParkingPriceService $parkingPriceService): JsonResource|Response
     {
+        if ($parking->stop_time) {
+            return response()->json(['errors' => ['general' => ['Parking already stopped.']], ]
+            , Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
         $parking->update([
             'stop_time' => now(),
             'total_price' => $parkingPriceService->calculatePrice($parking->zone_id, $parking->start_time, $parking->stop_time),
